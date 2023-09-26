e12092035264fbbab039e606e3c54a8ad242e957
ismv00 <ismv00@icloud.com>
2023-09-25 23:04:34 -0400

    first commit

---
+++ CursoTiagoAguiar/CursoTiagoAguiarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoTiagoAguiarApp.swift
+//  CursoTiagoAguiar
+//
+//  Created by Igor S. Menezes on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CursoTiagoAguiarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
